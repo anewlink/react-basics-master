996124299ec583b2fae2c04b83b7161e24405a76
Gabriel Martinez <grojas22@ford.com>
2025-01-16 15:57:48 -0500

    Initial project. index2.html was the original index.html file

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+      // React.createElement(type,[props], [...children])
+  // const button = React.createElement('button',{type: 'button'}, 'Save');
+  const button = <button type='button'>Save</button>;
+  /* const articleHeading = React.createElement('h2', null, 'My first blog post');
+  const paragraph = React.createElement('p', {className: 'text'}, 'This is the content of my post');
+  const article = React.createElement('article', null, articleHeading, paragraph);
+  const mainHeading = React.createElement('h1', null, 'My Blog');
+  const main = React.createElement('main', {id: 'main'}, mainHeading, article);*/
+function App() {
+  const articleHeading = React.createElement('h2', null, 'My first blog post');
+  const paragraph = React.createElement('p', {className: 'text'}, 'This is the content of my post');
+  const article = React.createElement('article', null, articleHeading, paragraph);
+  const mainHeading = React.createElement('h1', null, 'My Blog');
+  const main = React.createElement('main', {id: 'main'}, mainHeading, article);
+  return (
+    //button
+    main
+  );
+}
+
+
+/*function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+export default App;
